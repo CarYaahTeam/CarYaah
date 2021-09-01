2a307e0f115c5189afe3f5977d9b3b535d7f6b3a
Nidhalbenhajsalah <nidal86@gmail.com>
2021-09-01 19:17:25 +0100

    erging

--- CarYaah-project/src/app/carcard/carcard.component.ts
+++ CarYaah-project/src/app/carcard/carcard.component.ts
@@ -13,24 +13,18 @@ export class CarcardComponent implements OnInit {
   rangevalue=0;
   cars: any = [];
   saveCars : any = [];
-
   marked: any = []
 
   sedan_marked = false;
   sedan_checkbox = false;
-
   van_marked = false;
   van_checkbox = false;
-
   pickup_marked = false;
   pickup_checkbox = false;
-
   minitruck_marked = false;
   minitruck_checkbox = false;
-
   ac_marked = false;
   ac_checkbox = false;
-
   gps_marked = false;
   gps_checkbox = false;
 
@@ -64,88 +58,46 @@ export class CarcardComponent implements OnInit {
     } else {
       this.marked.push(type)
     }
-    console.log(this.marked)
     const storage: any = []
     this.marked.forEach((element: string) => {
       this.saveCars.forEach((car: any) => {
         car.type.toUpperCase() === element.toUpperCase() ? storage.push(car) : null
       });
-
     });
     this.cars = this.marked.length ? storage : this.saveCars
   }
-  // sedan(e:any){
-  //   this.sedan_marked= e.target.checked;
-  //   this.cars = this.saveCars
-  //   var storage = []
-  //   for(var i = 0 ; i < this.cars.length ; i++){
-  //     if(this.cars[i].type === 'sedan' && e.target.checked ){
-  //        storage.push(this.cars[i])
-  //     }
-  //   }
-  //   this.cars = storage
-  // }
 
-  // van(e:any){
-  //   this.van_marked= e.target.checked;
-  //   this.cars = this.saveCars
-  //   var storage = []
-  //   for(var i = 0 ; i < this.cars.length ; i++){
-  //     if(this.cars[i].type === 'van' && e.target.checked ){
-  //        storage.push(this.cars[i])
-  //     }
-  //   }
-  //   this.cars = storage
-  // }
+  ac(e:any){
+    this.ac_marked=e.target.checked;
+    this.cars = this.saveCars
+    var storage = []
+    for(var i = 0 ; i < this.cars.length ; i++){
+      if(this.cars[i].AC && e.target.checked ){
+        console.log(this.cars[i].AC);
+        
+         storage.push(this.cars[i])
+      }
+      else{
+        storage=this.cars
+      }
+    }
+    this.cars = storage
+  }
 
-  // pickup(e:any){
-  //   this.pickup_marked= e.target.checked;
-  //   this.cars = this.saveCars
-  //   var storage = []
-  //   for(var i = 0 ; i < this.cars.length ; i++){
-  //     if(this.cars[i].type === 'pickup' && e.target.checked ){
-  //        storage.push(this.cars[i])
-  //     }
-  //   }
-  //   this.cars = storage
-  // }
-
-  // minitruck(e:any){
-  //   this.minitruck_marked= e.target.checked;
-  //   this.cars = this.saveCars
-  //   var storage = []
-  //   for(var i = 0 ; i < this.cars.length ; i++){
-  //     if(this.cars[i].type === 'minitruck' && e.target.checked ){
-  //        storage.push(this.cars[i])
-  //     }
-  //   }
-  //   this.cars = storage
-  // }
-
-  // ac(e:any){
-  //   this.ac_marked=e.target.checked;
-  //   this.cars = this.saveCars
-  //   var storage = []
-  //   for(var i = 0 ; i < this.cars.length ; i++){
-  //     if(this.cars[i].AC && e.target.checked ){
-  //        storage.push(this.cars[i])
-  //     }
-  //   }
-  //   this.cars = storage
-  // }
-
-  // gps(e:any){
-  //   this.gps_marked=e.target.checked;
-  //   this.cars = this.saveCars
-  //   var storage = []
-  //   for(var i = 0 ; i < this.cars.length ; i++){
-  //     if(this.cars[i].GPS && e.target.checked ){
-  //        storage.push(this.cars[i])
-  //     }
-  //   }
-  //   this.cars = storage
-  // }
-
-
-  
+  gps(e:any){
+    this.gps_marked=e.target.checked;
+    this.cars = this.saveCars
+    var storage = []
+    for(var i = 0 ; i < this.cars.length ; i++){
+      if(this.cars[i].GPS && e.target.checked ){
+         storage.push(this.cars[i])
+         console.log(this.cars[i].GPS);
+      }
+      else{
+        storage=this.cars
+        console.log(this.cars[i].GPS);
+      }
+    }
+    this.cars = storage
+  } 
 }
