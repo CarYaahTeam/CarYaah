9272a18ff4dd6493b6c83fc1d377a296b037c06d
nomane-sabrine <nomane.sabrina@gmail.com>
2021-09-02 10:12:45 +0100

    crud owner

--- CarYaah-project/server/routers/ownerRoutes.js
+++
@@ -1,5 +0,0 @@
-var ownerRouter = require("express").Router();
-var ownercontrollers = require("../controllers/ownercontrollers.js");
-
-ownerRouter.post("/owner", ownercontrollers.createOwnerCar);
-module.exports = ownerRouter;
